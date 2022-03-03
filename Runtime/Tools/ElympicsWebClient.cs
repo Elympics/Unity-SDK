ebea12b717f4e6b61600665e2b93c849479f3b5c
Piotr Przestrzelski <piotr.przestrzelski@daftmobile.com>
2022-03-03 17:22:36 +0100

    Fixed againg pipeline for server - now synchronous

--- Runtime/Tools/ElympicsWebClient.cs
+++ Runtime/Tools/ElympicsWebClient.cs
@@ -17,7 +17,7 @@ namespace Elympics
 	public static class ElympicsWebClient
 	{
 #if UNITY_EDITOR
-		public static void SendEnginePostRequestApi(string url, string gameVersion, string[] filesPath, Action<UnityWebRequest> completed)
+		public static UnityWebRequestAsyncOperation SendEnginePostRequestApi(string url, string gameVersion, string[] filesPath, Action<UnityWebRequest> completed = null)
 		{
 			var formData = new List<IMultipartFormSection>();
 			foreach (var path in filesPath)
@@ -30,18 +30,21 @@ namespace Elympics
 			AcceptTestCertificateHandler.SetOnRequestIfNeeded(request);
 
 			var asyncOperation = request.SendWebRequest();
-			AttachCompletedCallback(completed, asyncOperation);
+			AttachCompletedCallback(asyncOperation, completed);
+			return asyncOperation;
 		}
 
-		private static void AttachCompletedCallback(Action<UnityWebRequest> completed, UnityWebRequestAsyncOperation asyncOperation)
+		private static void AttachCompletedCallback(UnityWebRequestAsyncOperation asyncOperation, Action<UnityWebRequest> completed = null)
 		{
+			if (completed == null)
+				return;
 			if (asyncOperation.isDone)
 				completed.Invoke(asyncOperation.webRequest);
 			else
 				asyncOperation.completed += _ => completed?.Invoke(asyncOperation.webRequest);
 		}
 
-		public static void SendJsonGetRequestApi(string url, Action<UnityWebRequest> completed)
+		public static UnityWebRequestAsyncOperation SendJsonGetRequestApi(string url, Action<UnityWebRequest> completed = null)
 		{
 			var uri = new Uri(url);
 			var request = UnityWebRequest.Get(uri);
@@ -55,13 +58,15 @@ namespace Elympics
 			Debug.Log($"[Elympics] Sending request GET {url}");
 
 			var asyncOperation = request.SendWebRequest();
-			AttachCompletedCallback(completed, asyncOperation);
+			AttachCompletedCallback(asyncOperation, completed);
+			return asyncOperation;
 		}
 
-		public static void SendJsonPostRequestApi(string url, object body, Action<UnityWebRequest> completed, bool auth = true)
+		public static UnityWebRequestAsyncOperation SendJsonPostRequestApi(string url, object body, Action<UnityWebRequest> completed = null, bool auth = true)
 		{
 			var asyncOperation = SendJsonPostRequest(url, body, auth ? $"Bearer {ElympicsConfig.AuthToken}" : null);
-			AttachCompletedCallback(completed, asyncOperation);
+			AttachCompletedCallback(asyncOperation, completed);
+			return asyncOperation;
 		}
 #endif
 

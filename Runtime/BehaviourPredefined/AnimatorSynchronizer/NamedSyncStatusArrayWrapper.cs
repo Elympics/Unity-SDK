Elympics/Unity-SDK

commit 864a13fef6ed69993a4a6466af24ff3d984582a9

Fix ElympicsAnimatorSynchronizerEditor not preserving state

Runtime/BehaviourPredefined/AnimatorSynchronizer/NamedSyncStatusArrayWrapper.cs
@@ -1,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace Elympics
+{
+	public class NamedSyncStatusArrayWrapper
+	{
+		public IEnumerable<NamedSynchronizationStatus> Elements
+		{
+			get
+			{
+				LoadCache();
+				return _nameList.Select(x => new NamedSynchronizationStatus { Name = x, Enabled = !_disabledNameSet.Contains(x) });
+			}
+		}
+
+		private readonly SerializedProperty _arrayProperty;
+		private readonly List<string> _nameList = new List<string>();
+
+		// temporary
+		private readonly HashSet<string> _disabledNameSet = new HashSet<string>();
+		private readonly HashSet<string> _newNameSet      = new HashSet<string>();
+
+		public NamedSyncStatusArrayWrapper(SerializedProperty arrayProperty)
+		{
+			_arrayProperty = arrayProperty;
+		}
+
+		public void UpdateList(IEnumerable<string> names)
+		{
+			_nameList.Clear();
+			_newNameSet.Clear();
+			foreach (var name in names)
+			{
+				_newNameSet.Add(name);
+				_nameList.Add(name);
+			}
+
+			LoadCache();
+			var previousCount = _disabledNameSet.Count;
+			_disabledNameSet.RemoveWhere(x => !_newNameSet.Contains(x));
+			if (_disabledNameSet.Count != previousCount)
+				WriteBackCache();
+		}
+
+		public void SetEnabled(string name, bool isEnabled)
+		{
+			for (var i = 0; i < _arrayProperty.arraySize; i++)
+			{
+				if (_arrayProperty.GetArrayElementAtIndex(i).stringValue != name)
+					continue;
+				if (isEnabled)
+					_arrayProperty.DeleteArrayElementAtIndex(i);
+				return;
+			}
+
+			var newIndex = _arrayProperty.arraySize;
+			_arrayProperty.InsertArrayElementAtIndex(newIndex);
+			_arrayProperty.GetArrayElementAtIndex(newIndex).stringValue = name;
+		}
+
+		private void LoadCache()
+		{
+			_disabledNameSet.Clear();
+			foreach (SerializedProperty element in _arrayProperty)
+				_disabledNameSet.Add(element.stringValue);
+		}
+
+		private void WriteBackCache()
+		{
+			_arrayProperty.ClearArray();
+			foreach (var name in _disabledNameSet)
+			{
+				var newIndex = _arrayProperty.arraySize;
+				_arrayProperty.InsertArrayElementAtIndex(newIndex);
+				_arrayProperty.GetArrayElementAtIndex(newIndex).stringValue = name;
+			}
+		}
+	}
+}

Runtime/BehaviourPredefined/AnimatorSynchronizer/ElympicsAnimatorSynchronizer.cs
@@ -8,25 +8,20 @@ namespace Elympics
 	[RequireComponent(typeof(Animator))]
 	public class ElympicsAnimatorSynchronizer : ElympicsMonoBehaviour, IInitializable, IStateSerializationHandler
 	{
-		[SerializeField, HideInInspector] private bool ShowLayerWeights = true;
+		[SerializeField, HideInInspector] private List<string> disabledParameters = new List<string>();
+		[SerializeField, HideInInspector] private List<string> disabledLayers     = new List<string>();
 
-		[SerializeField, HideInInspector] private bool ShowParameters = true;
-
-		[SerializeField, HideInInspector] private List<ParameterSynchronizationStatus> _boolStatuses = new List<ParameterSynchronizationStatus>();
-
-		[SerializeField, HideInInspector] private List<ParameterSynchronizationStatus> _intStatuses = new List<ParameterSynchronizationStatus>();
-
-		[SerializeField, HideInInspector] private List<ParameterSynchronizationStatus> _floatStatuses = new List<ParameterSynchronizationStatus>();
-
-		[SerializeField, HideInInspector] private List<ParameterSynchronizationStatus> _triggersStatuses = new List<ParameterSynchronizationStatus>();
-
-		[SerializeField, HideInInspector] private List<LayerSynchronizationStatus> _layersStatuses = new List<LayerSynchronizationStatus>();
+		private List<ParameterSynchronizationStatus> _boolStatuses;
+		private List<ParameterSynchronizationStatus> _intStatuses;
+		private List<ParameterSynchronizationStatus> _floatStatuses;
+		private List<ParameterSynchronizationStatus> _triggerStatuses;
+		private LayerSynchronizationStatus[]         _layerStatuses;
 
 		private ElympicsArray<ElympicsBool>  _boolParams;
 		private ElympicsArray<ElympicsInt>   _intParams;
 		private ElympicsArray<ElympicsFloat> _floatParams;
 		private ElympicsArray<ElympicsBool>  _triggerParams;
-		private ElympicsArray<ElympicsFloat> _layersWeights;
+		private ElympicsArray<ElympicsFloat> _layerWeights;
 
 		private readonly List<int> _triggerCache = new List<int>();
 
@@ -35,89 +30,30 @@ namespace Elympics
 
 		public void Initialize()
 		{
+			var parameters = Animator.parameters;
+			var layerNames = new string[Animator.layerCount];
+			for (var i = 0; i < layerNames.Length; i++)
+				layerNames[i] = Animator.GetLayerName(i);
+			_boolStatuses = new List<ParameterSynchronizationStatus>(parameters.Count(x => x.type == AnimatorControllerParameterType.Bool));
+			_intStatuses = new List<ParameterSynchronizationStatus>(parameters.Count(x => x.type == AnimatorControllerParameterType.Int));
+			_floatStatuses = new List<ParameterSynchronizationStatus>(parameters.Count(x => x.type == AnimatorControllerParameterType.Float));
+			_triggerStatuses = new List<ParameterSynchronizationStatus>(parameters.Count(x => x.type == AnimatorControllerParameterType.Trigger));
+			_layerStatuses = new LayerSynchronizationStatus[Animator.layerCount];
+
+			InitializeDeserializedStatuses(layerNames, parameters);
+
 			_boolParams = new ElympicsArray<ElympicsBool>(_boolStatuses.Count, () => new ElympicsBool());
 			_intParams = new ElympicsArray<ElympicsInt>(_intStatuses.Count, () => new ElympicsInt());
 			_floatParams = new ElympicsArray<ElympicsFloat>(_floatStatuses.Count, () => new ElympicsFloat());
-			_triggerParams = new ElympicsArray<ElympicsBool>(_triggersStatuses.Count, () => new ElympicsBool());
-			_layersWeights = new ElympicsArray<ElympicsFloat>(_layersStatuses.Count, () => new ElympicsFloat());
-		}
-
-		public void PrepareStatusesToUpdate()
-		{
-			_boolStatuses.ForEach(r => r.Updated = false);
-			_intStatuses.ForEach(r => r.Updated = false);
-			_floatStatuses.ForEach(r => r.Updated = false);
-			_triggersStatuses.ForEach(r => r.Updated = false);
-			_layersStatuses.ForEach(r => r.Updated = false);
-		}
-
-		public void RemoveOutdatedStatuses()
-		{
-			_boolStatuses.RemoveAll(r => r.Updated == false);
-			_intStatuses.RemoveAll(r => r.Updated == false);
-			_floatStatuses.RemoveAll(r => r.Updated == false);
-			_triggersStatuses.RemoveAll(r => r.Updated == false);
-			_layersStatuses.RemoveAll(r => r.Updated == false);
-		}
-
-		public void SetLayer(int index, bool isEnabled) => _layersStatuses.First(r => r.Index == index).Enabled = isEnabled;
-
-		public bool GetLayer(int layerIndex)
-		{
-			int index = _layersStatuses.FindIndex(r => r.Index == layerIndex);
-			if (index == -1)
-			{
-				_layersStatuses.Add(new LayerSynchronizationStatus { Enabled = true, Index = layerIndex, Updated = true });
-				return true;
-			}
-
-			_layersStatuses[index].Updated = true;
-			return _layersStatuses[index].Enabled;
-		}
-
-		public void SetParameter(AnimatorControllerParameterType type, int hashName, bool isEnabled)
-		{
-			var statuses = GetListByParameterType(type);
-			statuses.First(r => r.HashName == hashName).Enabled = isEnabled;
-		}
-
-		public bool GetParameter(AnimatorControllerParameterType type, int hashName)
-		{
-			var statuses = GetListByParameterType(type);
-			int index = statuses.FindIndex(x => x.HashName == hashName);
-
-			if (index == -1)
-			{
-				statuses.Add(new ParameterSynchronizationStatus { Enabled = true, HashName = hashName, Updated = true });
-				return true;
-			}
-
-			statuses[index].Updated = true;
-			return statuses[index].Enabled;
-		}
-
-		private List<ParameterSynchronizationStatus> GetListByParameterType(AnimatorControllerParameterType type)
-		{
-			switch (type)
-			{
-				case AnimatorControllerParameterType.Bool:
-					return _boolStatuses;
-				case AnimatorControllerParameterType.Int:
-					return _intStatuses;
-				case AnimatorControllerParameterType.Float:
-					return _floatStatuses;
-				case AnimatorControllerParameterType.Trigger:
-					return _triggersStatuses;
-				default:
-					return null;
-			}
+			_triggerParams = new ElympicsArray<ElympicsBool>(_triggerStatuses.Count, () => new ElympicsBool());
+			_layerWeights = new ElympicsArray<ElympicsFloat>(_layerStatuses.Length, () => new ElympicsFloat());
 		}
 
 		private void Update()
 		{
-			for (int i = 0; i < _triggersStatuses.Count; i++)
+			for (int i = 0; i < _triggerStatuses.Count; i++)
 			{
-				var triggerKey = _triggersStatuses[i];
+				var triggerKey = _triggerStatuses[i];
 				if (Animator.GetBool(triggerKey.HashName) && !_triggerCache.Contains(triggerKey.HashName))
 					_triggerCache.Add(triggerKey.HashName);
 			}
@@ -125,8 +61,8 @@ namespace Elympics
 
 		public void OnPreStateSerialize()
 		{
-			for (int i = 0; i < _layersStatuses.Count; i++)
-				_layersWeights.Values[i].Value = Animator.GetLayerWeight(i);
+			for (int i = 0; i < _layerStatuses.Length; i++)
+				_layerWeights.Values[i].Value = Animator.GetLayerWeight(i);
 
 			for (int i = 0; i < _boolStatuses.Count; i++)
 				_boolParams.Values[i].Value = Animator.GetBool(_boolStatuses[i].HashName);
@@ -137,17 +73,17 @@ namespace Elympics
 			for (int i = 0; i < _intStatuses.Count; i++)
 				_intParams.Values[i].Value = Animator.GetInteger(_intStatuses[i].HashName);
 
-			for (int i = 0; i < _triggersStatuses.Count; i++)
-				_triggerParams.Values[i].Value = _triggerCache.Contains(_triggersStatuses[i].HashName);
+			for (int i = 0; i < _triggerStatuses.Count; i++)
+				_triggerParams.Values[i].Value = _triggerCache.Contains(_triggerStatuses[i].HashName);
 
 			_triggerCache.Clear();
 		}
 
 		public void OnPostStateDeserialize()
 		{
-			for (int i = 0; i < _layersStatuses.Count; i++)
-				if (_layersStatuses[i].Enabled)
-					Animator.SetLayerWeight(i, _layersWeights.Values[i]);
+			for (int i = 0; i < _layerStatuses.Length; i++)
+				if (_layerStatuses[i].Enabled)
+					Animator.SetLayerWeight(i, _layerWeights.Values[i]);
 
 			for (int i = 0; i < _boolStatuses.Count; i++)
 				if (_boolStatuses[i].Enabled)
@@ -161,9 +97,50 @@ namespace Elympics
 				if (_intStatuses[i].Enabled)
 					Animator.SetInteger(_intStatuses[i].HashName, _intParams.Values[i].Value);
 
-			for (int i = 0; i < _triggersStatuses.Count; i++)
-				if (_triggerParams.Values[i].Value && _triggersStatuses[i].Enabled)
-					Animator.SetTrigger(_triggersStatuses[i].HashName);
+			for (int i = 0; i < _triggerStatuses.Count; i++)
+				if (_triggerParams.Values[i].Value && _triggerStatuses[i].Enabled)
+					Animator.SetTrigger(_triggerStatuses[i].HashName);
+		}
+
+		private void InitializeDeserializedStatuses(string[] layerNames, AnimatorControllerParameter[] parameters)
+		{
+			var disabledLayersSet = new HashSet<string>(disabledLayers);
+			var disabledParametersSet = new HashSet<string>(disabledParameters);
+
+			for (var i = 0; i < layerNames.Length; i++)
+				_layerStatuses[i] = new LayerSynchronizationStatus { Name = layerNames[i], Enabled = !disabledLayersSet.Contains(layerNames[i]) };
+
+			_boolStatuses.Clear();
+			_intStatuses.Clear();
+			_floatStatuses.Clear();
+			_triggerStatuses.Clear();
+			for (var i = 0; i < parameters.Length; i++)
+			{
+				var mappedParameter = new ParameterSynchronizationStatus
+				{
+					Name = parameters[i].name,
+					HashName = parameters[i].nameHash,
+					Enabled = !disabledParametersSet.Contains(parameters[i].name)
+				};
+				GetStatusesForType(parameters[i].type).Add(mappedParameter);
+			}
+		}
+
+		private List<ParameterSynchronizationStatus> GetStatusesForType(AnimatorControllerParameterType type)
+		{
+			switch (type)
+			{
+				case AnimatorControllerParameterType.Bool:
+					return _boolStatuses;
+				case AnimatorControllerParameterType.Int:
+					return _intStatuses;
+				case AnimatorControllerParameterType.Float:
+					return _floatStatuses;
+				case AnimatorControllerParameterType.Trigger:
+					return _triggerStatuses;
+				default:
+					return null;
+			}
 		}
 	}
 }

Runtime/BehaviourPredefined/AnimatorSynchronizer/ElympicsAnimatorSynchronizerEditor.cs
@@ -1,4 +1,6 @@
 ﻿#if UNITY_EDITOR
+using Plugins.Elympics.Plugins.ParrelSync;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.Animations;
 using UnityEngine;
@@ -9,89 +11,121 @@ namespace Elympics
 	internal partial class ElympicsAnimatorSynchronizerEditor : Editor
 	{
 		private ElympicsAnimatorSynchronizer _animatorSynchronizer;
-		private AnimatorController           _controller;
-		private Animator                     _animator;
+		private AnimatorController _controller;
+		private Animator _animator;
 
-		private GUIStyle summaryLabelStyle;
+		private NamedSyncStatusArrayWrapper _disabledParameters;
+		private NamedSyncStatusArrayWrapper _disabledLayers;
+
+		private bool _showLayerWeights = true;
+		private bool _showParameters   = true;
+
+		private GUIStyle _summaryLabelStyle;
 
 		private void OnEnable()
 		{
-			_animatorSynchronizer = (ElympicsAnimatorSynchronizer)this.target;
+			_animatorSynchronizer = (ElympicsAnimatorSynchronizer)target;
 			_animator = _animatorSynchronizer.GetComponent<Animator>();
 
 			if (_animator)
-				_controller = this.GetEffectiveController(_animator) as AnimatorController;
+				_controller = GetEffectiveController(_animator) as AnimatorController;
+
+			_disabledParameters = new NamedSyncStatusArrayWrapper(serializedObject.FindProperty("disabledParameters"));
+			_disabledLayers = new NamedSyncStatusArrayWrapper(serializedObject.FindProperty("disabledLayers"));
+			UpdateLayersAndParameters();
 		}
 
 		public override void OnInspectorGUI()
 		{
-			summaryLabelStyle = new GUIStyle(GUI.skin.label) { fontSize = 11, fontStyle = FontStyle.Italic, wordWrap = true };
+			_summaryLabelStyle = new GUIStyle(GUI.skin.label) { fontSize = 11, fontStyle = FontStyle.Italic, wordWrap = true };
 			base.OnInspectorGUI();
 
+			var isPlayingOrClone = EditorApplication.isPlaying || ElympicsClonesManager.IsClone();
+
 			if (_animator == null)
+			{
 				EditorGUILayout.HelpBox(Label_GameObjectNullWarning, MessageType.Warning);
+				return;
+			}
 
-			if (GetLayerCount() == 0)
-				EditorGUILayout.HelpBox(Label_NoAnimatorLayersWarning, MessageType.Warning);
+			if (_controller == null)
+			{
+				EditorGUILayout.HelpBox(Label_AnimatorControllerMissingWarning, MessageType.Warning);
+				return;
+			}
 
-			if (GetParameterCount() == 0)
-				EditorGUILayout.HelpBox(Label_NoAnimatorParametersWarning, MessageType.Warning);
+			if (EditorApplication.isPlaying)
+				EditorGUILayout.HelpBox(Label_PlayModeModificationWarning, MessageType.Warning);
+			if (ElympicsClonesManager.IsClone())
+				EditorGUILayout.HelpBox(Label_CloneModificationWarning, MessageType.Warning);
 
-			_animatorSynchronizer.PrepareStatusesToUpdate();
+			EditorGUI.BeginDisabledGroup(isPlayingOrClone);
+
+			if (GUILayout.Button(Label_RefreshButton))
+				UpdateLayersAndParameters();
+
+			EditorGUI.BeginChangeCheck();
 			DrawLayers();
 			EditorGUILayout.Space();
 			DrawParameters();
-			_animatorSynchronizer.RemoveOutdatedStatuses();
+			if (EditorGUI.EndChangeCheck())
+				serializedObject.ApplyModifiedProperties();
 
+			EditorGUI.EndDisabledGroup();
+		}
+
+		private void UpdateLayersAndParameters()
+		{
+			_disabledLayers.UpdateList(_controller.layers.Select(x => x.name));
+			_disabledParameters.UpdateList(_controller.parameters.Select(x => x.name));
 			serializedObject.ApplyModifiedProperties();
 		}
 
-
 		private void DrawLayers()
 		{
-			SerializedProperty showLayersWeight = this.serializedObject.FindProperty(ShowLayersWeightPropertyName);
-			showLayersWeight.boolValue = EditorGUILayout.Foldout(showLayersWeight.boolValue, Label_Layers);
-			EditorGUILayout.LabelField(Label_LayersTooltip, summaryLabelStyle);
+			_showLayerWeights = EditorGUILayout.Foldout(_showLayerWeights, Label_Layers);
+			EditorGUILayout.LabelField(Label_LayersTooltip, _summaryLabelStyle);
 
-			if (showLayersWeight.boolValue)
-				for (int i = 0; i < _controller.layers.Length; i++)
-					DrawLayer(_controller.layers[i], i);
-		}
+			if (_controller.layers.Length == 0)
+			{
+				EditorGUILayout.HelpBox(Label_NoAnimatorLayersWarning, MessageType.Warning);
+				return;
+			}
 
-		private void DrawLayer(AnimatorControllerLayer layer, int index)
-		{
-			EditorGUILayout.BeginVertical();
-			EditorGUILayout.BeginHorizontal();
-			var changedValue = EditorGUILayout.Toggle(new GUIContent(layer.name), _animatorSynchronizer.GetLayer(index));
-			_animatorSynchronizer.SetLayer(index, changedValue);
-			EditorGUILayout.EndHorizontal();
-			EditorGUILayout.EndVertical();
+			if (_showLayerWeights)
+				DrawNamedSyncStatusArray(_disabledLayers);
 		}
 
 		private void DrawParameters()
 		{
-			SerializedProperty showParameters = this.serializedObject.FindProperty(ShowParametersPropertyName);
-			showParameters.boolValue = EditorGUILayout.Foldout(showParameters.boolValue, Label_Parameters);
-			EditorGUILayout.LabelField(Label_ParametersTooltip, summaryLabelStyle);
+			_showParameters = EditorGUILayout.Foldout(_showParameters, Label_Parameters);
+			EditorGUILayout.LabelField(Label_ParametersTooltip, _summaryLabelStyle);
 
-			if (showParameters.boolValue)
-				for (int i = 0; i < _controller.parameters.Length; i++)
-					DrawParameter(_controller.parameters[i], i);
+			if (_controller.parameters.Length == 0)
+			{
+				EditorGUILayout.HelpBox(Label_NoAnimatorParametersWarning, MessageType.Warning);
+				return;
+			}
+
+			if (_showParameters)
+				DrawNamedSyncStatusArray(_disabledParameters);
 		}
 
-		private void DrawParameter(AnimatorControllerParameter parameter, int index)
+		private static void DrawNamedSyncStatusArray(NamedSyncStatusArrayWrapper arrayWrapper)
 		{
-			EditorGUILayout.BeginVertical();
-			EditorGUILayout.BeginHorizontal();
-			var parameterMode = _animatorSynchronizer.GetParameter(_controller.parameters[index].type, Animator.StringToHash(parameter.name));
-			var changedValue = EditorGUILayout.Toggle(new GUIContent(parameter.name), parameterMode);
-			_animatorSynchronizer.SetParameter(_controller.parameters[index].type, Animator.StringToHash(parameter.name), changedValue);
-			EditorGUILayout.EndHorizontal();
-			EditorGUILayout.EndVertical();
+			foreach (var syncStatus in arrayWrapper.Elements)
+			{
+				EditorGUILayout.BeginVertical();
+				EditorGUILayout.BeginHorizontal();
+				var enabled = EditorGUILayout.Toggle(new GUIContent(syncStatus.Name), syncStatus.Enabled);
+				if (enabled != syncStatus.Enabled)
+					arrayWrapper.SetEnabled(syncStatus.Name, enabled);
+				EditorGUILayout.EndHorizontal();
+				EditorGUILayout.EndVertical();
+			}
 		}
 
-
-		private RuntimeAnimatorController GetEffectiveController(Animator animator)
+		private static RuntimeAnimatorController GetEffectiveController(Animator animator)
 		{
 			RuntimeAnimatorController controller = animator.runtimeAnimatorController;
 			AnimatorOverrideController overrideController = controller as AnimatorOverrideController;
@@ -103,9 +137,6 @@ namespace Elympics
 
 			return controller;
 		}
-
-		private int GetLayerCount()     => _controller == null ? 0 : _controller.layers.Length;
-		private int GetParameterCount() => _controller == null ? 0 : _controller.parameters.Length;
 	}
 }
 #endif

Runtime/BehaviourPredefined/AnimatorSynchronizer/ElympicsAnimatorSynchronizerEditorLabels.cs
@@ -3,16 +3,19 @@
 	internal partial class ElympicsAnimatorSynchronizerEditor
 	{
 		private const string Label_GameObjectNullWarning = "GameObject doesn't have an Animator component to synchronize";
+		private const string Label_AnimatorControllerMissingWarning = "Cannot access AnimatorController instance";
 		private const string Label_NoAnimatorLayersWarning = "Animator doesn't have any layers to synchronize";
 		private const string Label_NoAnimatorParametersWarning = "Animator doesn't have any parameters to synchronize";
+
+		private const string Label_PlayModeModificationWarning = "Modification of settings in play mode is not allowed";
+		private const string Label_CloneModificationWarning = "Modification of settings in a clone is not allowed";
+
+		private const string Label_RefreshButton = "Refresh layers and parameters";
 
 		private const string Label_Layers = "Synchronized Layers";
 		private const string Label_LayersTooltip = "Choose which layers should be updated";
 
 		private const string Label_Parameters = "Synchronized Parameters";
 		private const string Label_ParametersTooltip = "Choose which parameters should be updated";
-
-		private const string ShowLayersWeightPropertyName = "ShowLayerWeights";
-		private const string ShowParametersPropertyName = "ShowParameters";
 	}
 }

Runtime/BehaviourPredefined/AnimatorSynchronizer/LayerSynchronizationStatus.cs
@@ -1,12 +1,8 @@
-﻿using System;
-
-namespace Elympics
+﻿namespace Elympics
 {
-	[Serializable]
 	public class LayerSynchronizationStatus
 	{
-		public int  Index;
-		public bool Enabled;
-		public bool Updated;
+		public string Name;
+		public bool   Enabled;
 	}
 }

Runtime/BehaviourPredefined/AnimatorSynchronizer/NamedSyncStatusArrayWrapper.cs.meta
@@ -1,5 +1,5 @@
 fileFormatVersion: 2
-guid: 916c2d31994623b4cae55a4fbb1a42be
+guid: fe12136ac2b176d4796f39ce7b286bfc
 MonoImporter:
   externalObjects: {}
   serializedVersion: 2

Runtime/BehaviourPredefined/AnimatorSynchronizer/NamedSynchronizationStatus.cs
@@ -1,0 +1,11 @@
+﻿using System;
+
+namespace Elympics
+{
+	[Serializable]
+	public class NamedSynchronizationStatus
+	{
+		public string Name;
+		public bool   Enabled;
+	}
+}

Runtime/BehaviourPredefined/AnimatorSynchronizer/NamedSynchronizationStatus.cs.meta
@@ -1,5 +1,5 @@
 fileFormatVersion: 2
-guid: 916c2d31994623b4cae55a4fbb1a42be
+guid: cdeffae9c06324f4696390b947f5cd9b
 MonoImporter:
   externalObjects: {}
   serializedVersion: 2

commit e1b6e2d72cb56df94f97e5678bf79fbc2ff16c9c

Fix build failing after 864a13fef6ed69993a4a6466af24ff3d984582a9

Runtime/BehaviourPredefined/AnimatorSynchronizer/NamedSyncStatusArrayWrapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 
@@ -79,3 +80,4 @@ namespace Elympics
 		}
 	}
 }
+#endif
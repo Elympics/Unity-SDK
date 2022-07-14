e1b6e2d72cb56df94f97e5678bf79fbc2ff16c9c
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2022-07-14 12:32:11 +0200

    Fix build failing after 864a13fef6ed69993a4a6466af24ff3d984582a9

--- Runtime/BehaviourPredefined/AnimatorSynchronizer/NamedSyncStatusArrayWrapper.cs
+++ Runtime/BehaviourPredefined/AnimatorSynchronizer/NamedSyncStatusArrayWrapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 
@@ -79,3 +80,4 @@ namespace Elympics
 		}
 	}
 }
+#endif

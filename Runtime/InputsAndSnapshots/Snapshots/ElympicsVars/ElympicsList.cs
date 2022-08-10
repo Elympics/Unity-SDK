10f3af5df05e4157b009d39834cb7e5b57f91b02
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2022-08-10 17:17:41 +0200

    Initialize ElympicsList internal comparison object

--- Runtime/InputsAndSnapshots/Snapshots/ElympicsVars/ElympicsList.cs
+++ Runtime/InputsAndSnapshots/Snapshots/ElympicsVars/ElympicsList.cs
@@ -8,144 +8,142 @@ namespace Elympics
 	[Serializable]
 	public class ElympicsList<T> : ElympicsVar, IEnumerable<T> where T : ElympicsVar
 	{
-		private Func<T> factoryFunc = null;
-		private List<T> values;
+		private Func<T> _factoryFunc;
+		private List<T> _values;
 
-		private T listElementInstanceForDataCompare = null;
+		private T _listElementInstanceForDataCompare;
 
-		public int Count => values.Count;
+		public int Count => _values.Count;
 
 		public bool IsReadOnly => false;
 
-		public T this[int index] { get => values[index]; }
+		public T this[int index] => _values[index];
 
 		public ElympicsList(Func<T> factory, int elementsInListAtStart = 0, bool enableSynchronization = true) : base(enableSynchronization)
 		{
-			factoryFunc = factory;
-			listElementInstanceForDataCompare = factory();
-			
-			values = new List<T>();
+			_factoryFunc = factory;
+			_listElementInstanceForDataCompare = factory();
+
+			_values = new List<T>();
 			AddElementsToList(elementsInListAtStart);
 		}
 
 		public override void Serialize(BinaryWriter bw)
 		{
-			bw.Write(values.Count);
+			bw.Write(_values.Count);
 
-			foreach (var elympicsVar in values)
+			foreach (var elympicsVar in _values)
 				elympicsVar.Serialize(bw);
 		}
 
 		public override void Deserialize(BinaryReader br, bool ignoreTolerance = false)
 		{
-			int elementsInList = br.ReadInt32();
+			var elementsInList = br.ReadInt32();
 
-			if (elementsInList > values.Count)
-				AddElementsToList(elementsInList - values.Count);
-			else if (elementsInList < values.Count)
-				RemoveElementsFromList(values.Count - elementsInList);
+			if (elementsInList > _values.Count)
+				AddElementsToList(elementsInList - _values.Count);
+			else if (elementsInList < _values.Count)
+				RemoveElementsFromList(_values.Count - elementsInList);
 
-			foreach (var elympicsVar in values)
+			foreach (var elympicsVar in _values)
 				elympicsVar.Deserialize(br, ignoreTolerance);
 		}
-		
+
 		private void AddElementsToList(int elementsToAdd)
 		{
-			for (int i = 0; i < elementsToAdd; i++)
-			{
+			for (var i = 0; i < elementsToAdd; i++)
 				Add();
-			}
 		}
 
 		private void RemoveElementsFromList(int elementsToRemove)
 		{
-			values.RemoveRange(values.Count - elementsToRemove, elementsToRemove);
+			_values.RemoveRange(_values.Count - elementsToRemove, elementsToRemove);
 		}
 
 		public override bool Equals(BinaryReader br1, BinaryReader br2)
 		{
-			int br1data = br1.ReadInt32();
-			int br2data = br2.ReadInt32();
+			var list1Length = br1.ReadInt32();
+			var list2Length = br2.ReadInt32();
 
-			if (br1data != br2data)
+			if (list1Length != list2Length)
 				return false;
 
-			for (var i = 0; i < br1data; i++)
-			{
-				if (!listElementInstanceForDataCompare.Equals(br1, br2))
+			for (var i = 0; i < list1Length; i++)
+				if (!_listElementInstanceForDataCompare.Equals(br1, br2))
 					return false;
-			}
+
 			return true;
 		}
 
 		public int IndexOf(T item)
 		{
-			return values.IndexOf(item);
+			return _values.IndexOf(item);
 		}
 
 		public T Insert(int index)
 		{
-			T createdInstance = CreateAndInitialize();
-			values.Insert(index, createdInstance);
+			var createdInstance = CreateAndInitialize();
+			_values.Insert(index, createdInstance);
 
 			return createdInstance;
 		}
 
 		public void RemoveAt(int index)
 		{
-			values.RemoveAt(index);
+			_values.RemoveAt(index);
 		}
 
 		public T Add()
 		{
-			T createdInstance = CreateAndInitialize();
-			values.Add(createdInstance);
+			var createdInstance = CreateAndInitialize();
+			_values.Add(createdInstance);
 
 			return createdInstance;
 		}
 
 		private T CreateAndInitialize()
 		{
-			T createdInstance = factoryFunc();
+			var createdInstance = _factoryFunc();
 			createdInstance.Initialize(Elympics);
 			return createdInstance;
 		}
 
 		public void Clear()
 		{
-			values.Clear();
+			_values.Clear();
 		}
 
 		public bool Contains(T item)
 		{
-			return values.Contains(item);
+			return _values.Contains(item);
 		}
 
 		public void CopyTo(T[] array, int arrayIndex)
 		{
-			values.CopyTo(array, arrayIndex);
+			_values.CopyTo(array, arrayIndex);
 		}
 
 		public bool Remove(T item)
 		{
-			return values.Remove(item);
+			return _values.Remove(item);
 		}
 
 		public IEnumerator<T> GetEnumerator()
 		{
-			return values.GetEnumerator();
+			return _values.GetEnumerator();
 		}
 
 		IEnumerator IEnumerable.GetEnumerator()
 		{
-			return values.GetEnumerator();
+			return _values.GetEnumerator();
 		}
 
-		internal override void Initialize(IElympics elympics) 
+		internal override void Initialize(IElympics elympics)
 		{
 			base.Initialize(elympics);
+			_listElementInstanceForDataCompare.Initialize(elympics);
 
-			foreach (T element in values)
+			foreach (var element in _values)
 				element.Initialize(elympics);
 		}
 	}

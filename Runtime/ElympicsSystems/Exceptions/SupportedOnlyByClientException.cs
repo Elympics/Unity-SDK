99358da525e9caa635ef8a3c1b616c90213be75d
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2022-08-26 13:13:25 +0200

    Expose TicksPerSecond property in IElympics

---
+++ Runtime/ElympicsSystems/Exceptions/SupportedOnlyByClientException.cs
@@ -0,0 +1,13 @@
+namespace Elympics
+{
+	public class SupportedOnlyByClientException : ElympicsException
+	{
+		private const string DefaultMessage = "This method is supported only for client";
+
+		public SupportedOnlyByClientException() : base(DefaultMessage)
+		{ }
+
+		public SupportedOnlyByClientException(string message) : base(message)
+		{ }
+	}
+}

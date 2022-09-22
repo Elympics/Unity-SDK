3e545111ec5ef1a3a42f57901a5993766e359753
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2022-09-22 12:13:24 +0000

    Accept API URLs with non-empty path

---
+++ Tests/Editor/TestEndpointChecker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Elympics;
+using NUnit.Framework;
+
+namespace Tests
+{
+	public class TestEndpointChecker
+	{
+		private EditorEndpointChecker _endpointChecker;
+
+		private static IEnumerable<TestAddress> _testAddresses => new List<TestAddress>()
+		{
+			new TestAddress("https", false),
+			new TestAddress("https:", false),
+			new TestAddress("https://", false),
+			new TestAddress("https://test", true),
+			new TestAddress("https://test.domain/", true),
+			new TestAddress("https://test.domain/path", true),
+			new TestAddress("https://test.domain/path/", true)
+		};
+
+		[SetUp]
+		public void SetUp()
+		{
+			_endpointChecker = new EditorEndpointChecker();
+		}
+
+		[Test]
+		public void TestUrlValidation([ValueSource(nameof(_testAddresses))] TestAddress testAddress)
+		{
+			_endpointChecker.UpdateUri(testAddress.Address);
+			Assert.AreEqual(_endpointChecker.IsUriCorrect, testAddress.IsCorrect);
+		}
+
+		public class TestAddress
+		{
+			public string Address;
+			public bool IsCorrect;
+
+			public TestAddress(string address, bool isCorrect) => (Address, IsCorrect) = (address, isCorrect);
+		}
+	}
+}

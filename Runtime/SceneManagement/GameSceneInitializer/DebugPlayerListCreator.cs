8770070c18899fb9baf1b31a1423ae227ce70213
Jakub Motyka <jakub.motyka@daftmobile.com>
2022-09-22 11:23:53 +0000

    Fix auto generated duplicate user ids

--- Runtime/SceneManagement/GameSceneInitializer/DebugPlayerListCreator.cs
+++ Runtime/SceneManagement/GameSceneInitializer/DebugPlayerListCreator.cs
@@ -9,13 +9,9 @@ namespace Elympics
 	{
 		internal static List<InitialMatchUserData> CreatePlayersList(ElympicsGameConfig elympicsGameConfig)
 		{
-			var userIdSet = new HashSet<string>(elympicsGameConfig.TestPlayers.Select(x => x.userId));
-			if (userIdSet.Count != elympicsGameConfig.TestPlayers.Count)
-				throw new ArgumentException("User ids in test players should be unique!");
-
-			return elympicsGameConfig.TestPlayers.Select(initialUserData => new InitialMatchUserData
+			return elympicsGameConfig.TestPlayers.Select((initialUserData, index) => new InitialMatchUserData
 				{
-					UserId = initialUserData.userId,
+					UserId = index.ToString(),
 					IsBot = initialUserData.isBot,
 					BotDifficulty = initialUserData.botDifficulty,
 					MatchmakerData = initialUserData.matchmakerData,

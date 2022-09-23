7cbeaa63d193fbef80d0a4d57de9bd6255bf6d4b
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2022-09-23 09:03:41 +0200

    Restore text field accidentally removed in "Match events" sample

--- Samples~/MatchEvents/Scripts/CustomServerHandler.cs
+++ Samples~/MatchEvents/Scripts/CustomServerHandler.cs
@@ -18,6 +18,7 @@ namespace MatchEvents
 		private int _totalPlayers;
 		private int _totalBotPlayers;
 
+		[SerializeField] private ElympicsString text = new ElympicsString();
 		private readonly Dictionary<ElympicsPlayer, bool> _isBot = new Dictionary<ElympicsPlayer, bool>();
 		private readonly HashSet<ElympicsPlayer> _playersConnected = new HashSet<ElympicsPlayer>();
 		private readonly HashSet<ElympicsPlayer> _botPlayersConnected = new HashSet<ElympicsPlayer>();
@@ -91,7 +92,6 @@ namespace MatchEvents
 		private void UpdateText()
 		{
 			text.Value = $"Connected players: {_playersConnected.Count}\n(including {_botPlayersConnected.Count} bots)";
-			test.Value++;
 		}
 	}
 }

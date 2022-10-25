83b029e0c61d514ae87da46609a6c5c4a8c49776
Michał Dąbrowski <michal.dabrowski@daftmobile.com>
2022-10-25 20:47:44 +0000

    Add Unity Inspector warning when adding Transform and RigidBody synchronization components

---
+++ Runtime/BehaviourPredefined/CustomEditor/ElympicsRigidBody2DSynchronizerEditor.cs
@@ -0,0 +1,39 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Elympics
+{
+	[CustomEditor(typeof(ElympicsRigidBody2DSynchronizer))]
+	public class ElympicsRigidBody2DSynchronizerEditor : Editor
+	{
+		private GUIStyle _warningStyle;
+		private ElympicsRigidBody2DSynchronizer _target;
+		private string Label_TransformAsReduntandSynchronizerWarning = "<color=yellow>Warning! Behaviour contains RigidBody Synchronizer. Additional Transform synchronizer may cause performance and synchronization issues.</color> <a href=\"https://docs.elympics.cc/guide/state/#elympicsrigidbodysynchronizer\"></a>";
+
+		/* todo add LINK text in href RichText after after migrating to 2021.3 where Unity GUI is able to detect hyperlink text click ~kpieta 25.10.2022 https://docs.unity3d.com/2021.3/Documentation/ScriptReference/EditorGUI-hyperLinkClicked.html.*/
+
+		public override void OnInspectorGUI()
+		{
+			_warningStyle = new GUIStyle(GUI.skin.label) { fontSize = 11, fontStyle = FontStyle.Normal, wordWrap = true, richText = true };
+			if (_target == null)
+			{
+				_target = serializedObject.targetObject as ElympicsRigidBody2DSynchronizer;
+			}
+			if (_target.TryGetComponent<ElympicsTransformSynchronizer>(out _))
+			{
+				EditorGUILayout.Space(5);
+				EditorGUILayout.BeginVertical();
+				EditorGUILayout.LabelField(Label_TransformAsReduntandSynchronizerWarning, _warningStyle);
+
+				if (GUILayout.Button(ElympicsDocumentationUrls.Label_Documentation, GUILayout.ExpandWidth(false)))
+					Application.OpenURL(ElympicsDocumentationUrls.Link_TransfromSynchronizerDocumentation);
+
+				EditorGUILayout.EndVertical();
+				EditorGUILayout.Space(5);
+			}
+			base.OnInspectorGUI();
+		}
+	}
+}
+#endif
---
+++ Runtime/BehaviourPredefined/CustomEditor/ElympicsTransformSynchronizerEditor.cs
@@ -0,0 +1,41 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Elympics
+{
+	[CustomEditor(typeof(ElympicsTransformSynchronizer))]
+	public class ElympicsTransformSynchronizerEditor : Editor
+	{
+		private GUIStyle _warningStyle;
+		private ElympicsTransformSynchronizer _target;
+		private string Label_RigidBodyAsReduntandSynchronizerWarning = "<color=yellow>Warning! Behaviour contains Transform Synchronizer. Additional RigidBody synchronizer may cause performance and synchronization issues.</color> <a href=\"https://docs.elympics.cc/guide/state/#elympicsrigidbodysynchronizer\"></a>";
+
+		/* todo add LINK text in href RichText after after migrating to 2021.3 where Unity GUI is able to detect hyperlink text click ~kpieta 25.10.2022 https://docs.unity3d.com/2021.3/Documentation/ScriptReference/EditorGUI-hyperLinkClicked.html.*/
+
+		public override void OnInspectorGUI()
+		{
+			_warningStyle = new GUIStyle(GUI.skin.label) { fontSize = 11, fontStyle = FontStyle.Normal, wordWrap = true, richText = true };
+			if (_target == null)
+			{
+				_target = serializedObject.targetObject as ElympicsTransformSynchronizer;
+			}
+			if (_target.TryGetComponent<ElympicsRigidBodySynchronizer>(out _) || _target.TryGetComponent<ElympicsRigidBody2DSynchronizer>(out _))
+			{
+				EditorGUILayout.Space(5);
+				EditorGUILayout.BeginVertical();
+				EditorGUILayout.LabelField(Label_RigidBodyAsReduntandSynchronizerWarning, _warningStyle);
+
+				if (GUILayout.Button(ElympicsDocumentationUrls.Label_Documentation, GUILayout.ExpandWidth(false)))
+					Application.OpenURL(ElympicsDocumentationUrls.Link_RigidBodySynchronizerDocumentation);
+
+				EditorGUILayout.EndVertical();
+				EditorGUILayout.Space(5);
+			}
+			base.OnInspectorGUI();
+		}
+
+	}
+}
+
+#endif

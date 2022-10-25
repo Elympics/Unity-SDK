424faa10d6436ea46c668dc5b0c140af687087b8
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2022-10-25 23:48:08 +0200

    Select only distinct component types for emiting warnings regarding non-synchronized ElympicsVars

--- Editor/ElympicsEditorWarnings.cs
+++ Editor/ElympicsEditorWarnings.cs
@@ -1,37 +1,34 @@
-using System.Collections.Generic;
-using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor;
-using System.Reflection;
 
 namespace Elympics
 {
 	public static class ElympicsEditorWarnings
 	{
-		[UnityEditor.Callbacks.DidReloadScripts]
+		[DidReloadScripts]
 		private static void CheckElympicsVars()
 		{
-			var baseSynchronizationClassesInApplication = SceneObjectsFinder.FindObjectsOfType<IObservable>(SceneManager.GetActiveScene(), true);
-			foreach (var customClass in baseSynchronizationClassesInApplication)
+			var baseSynchronizationClassesInApplication = SceneObjectsFinder.FindObjectsOfType<IObservable>(SceneManager.GetActiveScene(), true)
+				.Select(obj => obj.GetType());
+			foreach (var customClass in baseSynchronizationClassesInApplication.Distinct())
 			{
-				var type = customClass.GetType();
+				var type = customClass;
 
 				while (typeof(IObservable).IsAssignableFrom(type.BaseType))
 				{
 					type = type.BaseType;
 
-					FieldInfo[] fields = type
-							.GetFields(
-							 BindingFlags.NonPublic |
-							 BindingFlags.Instance);
+					var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
 
 					foreach (var field in fields)
 					{
 						var isElympicsVar = typeof(ElympicsVar).IsAssignableFrom(field.FieldType);
 
 						if (field.IsPrivate && isElympicsVar)
-							Debug.LogWarning($"WARNING! Private ElympicsVars ({field}) in base {customClass} class aren't synchronized! Try making them protected instead.");
+							Debug.LogWarning($"WARNING! Private ElympicsVar ({field}) in base {type} of class {customClass} isn't synchronized! Try making it protected instead.");
 					}
 				}
 			}
--- Editor/ElympicsTools.cs
+++ Editor/ElympicsTools.cs
@@ -3,7 +3,6 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
-using System.Reflection;
 
 namespace Elympics
 {

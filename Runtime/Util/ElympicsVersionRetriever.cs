1bbb97df800d8ea1d457a99d66612912d745aced
Krzysztof Pięta <krzysztof.pieta@elympics.cc>
2023-02-01 19:39:34 +0000

    fix: Make sure Elympics-SDK-Version HTTP header is not empty

---
+++ Runtime/Util/ElympicsVersionRetriever.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Elympics
+{
+	public static class ElympicsVersionRetriever
+	{
+		private static string _elympicsName = "Elympics";
+
+		public static string GetVersionFromAssembly()
+		{
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+			return assemblies.Select(x => x.GetName()).FirstOrDefault(x => x.Name == _elympicsName)?.Version?.ToString(3) ?? string.Empty;
+		}
+	}
+}

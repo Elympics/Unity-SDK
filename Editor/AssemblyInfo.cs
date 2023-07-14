5277e4743fcb73df4e86be7b6e4756f816665365
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2023-07-15 00:30:16 +0200

    chore: Update Elympics.Editor AssemblyInfo

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("Elympics")]
 [assembly: AssemblyProduct("Elympics SDK")]
-[assembly: AssemblyVersion("0.7.3.0")]
+[assembly: AssemblyVersion("0.8.1.0")]
 [assembly: InternalsVisibleTo("Elympics.Editor.Tests")]

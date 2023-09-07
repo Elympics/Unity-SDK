7f928f8ef35298af3be4375c5778360b347cb84c
Dawid Sygocki <dawid.sygocki@daftmobile.com>
2023-09-07 10:19:21 +0200

    fix: Improve handling of duplicated ElympicsUnityPhysicsSimulator

--- Runtime/BehaviourPredefined/ElympicsUnityPhysicsSimulator.cs
+++ Runtime/BehaviourPredefined/ElympicsUnityPhysicsSimulator.cs
@@ -16,7 +16,7 @@ namespace Elympics
 
         public void Initialize()
         {
-            if (instance)
+            if (instance is not null && instance != this)
             {
                 ElympicsLogger.LogError("You can't use more than 1 instance of "
                     + $"{nameof(ElympicsUnityPhysicsSimulator)} in a single scene!\n"

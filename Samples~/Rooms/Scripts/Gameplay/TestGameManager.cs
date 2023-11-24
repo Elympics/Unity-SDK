0e8fe7cd9a112ba7fd05aa09c24d1bb32b4e34fb
Patryk Chojnicki <patryk.chojnicki@daftmobile.com>
2023-11-24 01:38:01 +0100

    Modify Rooms GameplayScene to work properly and not end the game instantly after leaving by any of the players (for testing of rejoin to match)

--- Samples~/Rooms/Scripts/Gameplay/TestGameManager.cs
+++ Samples~/Rooms/Scripts/Gameplay/TestGameManager.cs
@@ -5,21 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class TestGameManager : ElympicsMonoBehaviour, IClientHandlerGuid, IUpdatable
 {
-    private bool gameShouldEnd = false;
+    private readonly static string MenuScene = "RoomsMenu";
+
+    private bool _gameEndedClicked = false;
 
     [UsedImplicitly]
-    public void SetEndGame() => EndGameOnServer();
+    public void SetEndGame() => _gameEndedClicked = true;
 
     [ElympicsRpc(ElympicsRpcDirection.PlayerToServer)]
-    private void EndGameOnServer() => gameShouldEnd = true;
-
-    public void OnMatchEnded(Guid matchId) => Invoke(nameof(LoadMenuScene), 2f);
-
-    private void LoadMenuScene() => SceneManager.LoadScene(0);
+    private void EndGameOnServer() => _gameEndedClicked = true;
 
     public void ElympicsUpdate()
     {
-        if (Elympics.IsServer && gameShouldEnd)
+        if (!_gameEndedClicked)
+            return;
+
+        if (Elympics.IsClient)
+            EndGameOnServer();
+        else if (Elympics.IsServer)
             Elympics.EndGame();
+
+        _gameEndedClicked = false;
     }
+
+    private void LoadMenuScene() => SceneManager.LoadScene(MenuScene);
+
+    [UsedImplicitly] // TODO: remove implicit use from button when OnMatchEnded is fixed
+    public void GoBackToMenuAfterShortDelay() => Invoke(nameof(LoadMenuScene), 0.5f);
+
+    public void OnMatchEnded(Guid matchId) => GoBackToMenuAfterShortDelay();
 }

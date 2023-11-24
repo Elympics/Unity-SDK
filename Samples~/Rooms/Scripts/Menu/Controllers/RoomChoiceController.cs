d74a25ff586f22b5d55212d8f118dcfe28d184ec
Patryk Chojnicki <patryk.chojnicki@daftmobile.com>
2023-11-24 04:52:30 +0100

    Fix join code error messages not showing and throwin errors

--- Samples~/Rooms/Scripts/Menu/Controllers/RoomChoiceController.cs
+++ Samples~/Rooms/Scripts/Menu/Controllers/RoomChoiceController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Cysharp.Threading.Tasks;
 using Elympics;
 using JetBrains.Annotations;
 using UnityEngine;
@@ -83,6 +84,8 @@ public class RoomChoiceController : BaseWindow
         }
         catch (Exception e)
         {
+            await UniTask.SwitchToMainThread();
+
             var errorMessage = $"Joining failed: {e.Message}";
             Debug.LogError(errorMessage);
             errorPopup.SetMessage(errorMessage);

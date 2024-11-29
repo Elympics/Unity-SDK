4a98a03b5e996a95793b8fbb0947d74b93af4b79
Kacper Dudziak <kacper.dudziak@elympics.ai>
2024-11-29 12:49:22 +0100

    feat: remove IRng and UnityRandom

--- Runtime/InputsAndSnapshots/Snapshots/ElympicsVars/ElympicsRandom.cs
+++ Runtime/InputsAndSnapshots/Snapshots/ElympicsVars/ElympicsRandom.cs
@@ -5,7 +5,7 @@ using Random = Unity.Mathematics.Random;
 namespace Elympics
 {
     [Serializable]
-    public class ElympicsRandom : ElympicsVar<ElympicsRandomInternal>, IRng
+    public sealed class ElympicsRandom : ElympicsVar<ElympicsRandomInternal>
     {
         public override void Serialize(BinaryWriter bw) => bw.Write(Value.Rng.state);
 
@@ -15,36 +15,19 @@ namespace Elympics
 
         public int Range(int minInclusive, int maxInclusive) => Value.Rng.NextInt(minInclusive, maxInclusive);
 
-        public float value => Value.Rng.NextFloat();
+        public float NextFloat() => Value.Rng.NextFloat();
     }
 
     public readonly struct ElympicsRandomInternal : IEquatable<ElympicsRandomInternal>
     {
-        public readonly Random Rng;
+        internal readonly Random Rng;
 
-        public ElympicsRandomInternal(uint state) => Rng.state = state;
+        internal ElympicsRandomInternal(uint state) => Rng.state = state;
 
         public bool Equals(ElympicsRandomInternal other) => other.Rng.state == Rng.state;
 
         public override bool Equals(object obj) => obj is ElympicsRandomInternal other && Equals(other);
 
-        public override int GetHashCode() => Rng.GetHashCode();
-
-        public static implicit operator Random(ElympicsRandomInternal random) => random.Rng;
-        public static implicit operator ElympicsRandomInternal(Random random) => new(random.state);
-    }
-
-    public interface IRng
-    {
-        float Range(float minInclusive, float maxInclusive);
-        int Range(int minInclusive, int maxInclusive);
-        float value { get; }
-    }
-
-    public class UnityRandom : IRng
-    {
-        public float Range(float minInclusive, float maxInclusive) => UnityEngine.Random.Range(minInclusive, maxInclusive);
-        public int Range(int minInclusive, int maxInclusive) => UnityEngine.Random.Range(minInclusive, maxInclusive);
-        public float value => UnityEngine.Random.value;
+        public override int GetHashCode() => Rng.state.GetHashCode();
     }
 }

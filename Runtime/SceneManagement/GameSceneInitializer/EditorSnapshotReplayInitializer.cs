7395415cbcf647da0f5debbeaada5d295be0b36a
Kacper Dudziak <kacper.dudziak@elympics.ai>
2025-05-13 17:09:17 +0200

    feat: check game version when loading a replay

--- Runtime/SceneManagement/GameSceneInitializer/EditorSnapshotReplayInitializer.cs
+++ Runtime/SceneManagement/GameSceneInitializer/EditorSnapshotReplayInitializer.cs
@@ -18,12 +18,19 @@ namespace Elympics
             ElympicsGameConfig gameConfig,
             ElympicsBehavioursManager behavioursManager)
         {
+            var retriever = new EditorSnapshotAnalysisRetriever(gameConfig.SnapshotFilePath);
+            var replayVersion = retriever.RetrieveInitData().GameVersion;
+            var currentVersion = ElympicsConfig.LoadCurrentElympicsGameConfig().GameVersion;
+
+            if (replayVersion != currentVersion)
+                throw new Exception($"Game version mismatch. Replay was recorded using game version {replayVersion} and current game version is {currentVersion}. Use a matching version of the game to watch this replay.");
+
             InitializeInternal(
                 bot,
                 server,
                 gameConfig,
                 behavioursManager,
-                new EditorSnapshotAnalysisRetriever(gameConfig.SnapshotFilePath),
+                retriever,
                 (GetManipulator ?? throw new Exception($"{nameof(GetManipulator)} should always be set in editor, are you trying to use {nameof(EditorSnapshotReplayInitializer)} outside of editor?")).Invoke());
         }
     }

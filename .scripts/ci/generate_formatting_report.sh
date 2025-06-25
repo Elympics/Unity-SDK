ce23d414abc6e822f63407d18eed24f30d6efff8
Dawid Sygocki <dawid.sygocki@elympics.ai>
2025-06-25 08:08:51 +0200

    ci: Sort and preview formatting report in logs

--- .scripts/ci/generate_formatting_report.sh
+++ .scripts/ci/generate_formatting_report.sh
@@ -15,10 +15,20 @@ jq -r 'def make_fingerprint: .FilePath + ":" + (.LineNumber | tostring) + ":" + 
         "check_name": .DiagnosticId,
         "fingerprint": . | make_fingerprint,
         "severity": . | make_severity,
-        "location": { "path": .FilePath, "lines": { "begin": .LineNumber } }
-    })' ./format-report.json > ./format-report.codequality.json
+        "location": { "path": .FilePath, "lines": { "begin": .LineNumber, "begin_char": .CharNumber } }
+    })' ./format-report.json > ./format-report-unsorted.codequality.json
+jq -r 'def get_severity_index: if .severity == "critical" then 0 elif .severity == "major" then 1 else 2 end;
+  sort_by(. | get_severity_index, .location.path, .location.lines.begin)' ./format-report-unsorted.codequality.json > ./format-report.codequality.json
 sed -i 's@/Samples/@/Samples~/@' ./format-report.codequality.json
 sed -i 's@'"$PACKAGE_FOLDER"'@@i' ./format-report.codequality.json
 
+# Output summary to console
+GREEN='\033[0;32m'
+CLEAR='\033[0m'
+echo -e "${GREEN}Summarizing the formatting check...${CLEAR}"
+echo "===== BEGIN ====="
+jq -r '.[] | "[" + .severity  + "] " + .location.path + ":" + (.location.lines.begin | tostring) + ":" + (.location.lines.begin_char | tostring) + "; " + .description' ./format-report.codequality.json
+echo "=====  END  ====="
+
 # Fail on any warning
-dotnet format --no-restore --verify-no-changes --verbosity diagnostic --severity warn "./${TESTING_PROJECT_NAME}/${TESTING_PROJECT_NAME}.sln"
+dotnet format --no-restore --verify-no-changes --verbosity diagnostic --severity warn "./${TESTING_PROJECT_NAME}/${TESTING_PROJECT_NAME}.sln" > /dev/null 2> /dev/null

1769f36a5dc16a75be37bf1d0411efa7e6cd6757
Kacper Dudziak <kacper.dudziak@elympics.ai>
2025-07-11 15:00:57 +0200

    feat!: improve documentation and parameter names in RawCoinConverter

--- Runtime/Util/RawCoinConverter.cs
+++ Runtime/Util/RawCoinConverter.cs
@@ -10,24 +10,29 @@ namespace Elympics.Util
     /// Raw values are expressed in atomic native units of coins.
     /// Different coins have different scales for conversion between regular and raw units and use different names to refer to them.
     /// For example in case of ETH that raw unit is called Wei and 0.01 ETH equals 10000000000000000 Wei.
+    /// This conversion always happens by multiplying or dividing by powers of 10.
     /// </remarks>
     public static class RawCoinConverter
     {
-        /// <param name="decimalPlacesToUnit">Usually fetched from <see cref="CurrencyInfo.Decimals"/>.</param>
-        public static decimal FromRaw(string rawAmount, int decimalPlacesToUnit)
+        /// <param name="rawAmount">String containing numbers that represent amount of a coin in its atomic native units.</param>
+        /// <param name="decimals">Number of decimal places allowed by the coin. Usually fetched from <see cref="CurrencyInfo.Decimals"/>.</param>
+        /// <returns>User-friendly coin amount that can have up to <paramref name="decimals"/> decimal places of precision.</returns>
+        public static decimal FromRaw(string rawAmount, int decimals)
         {
             if (!BigInteger.TryParse(rawAmount, out var bigIntWei))
                 throw new ArgumentException($"Invalid amount string: {rawAmount}", nameof(rawAmount));
 
-            var divisor = BigInteger.Pow(10, decimalPlacesToUnit);
+            var divisor = BigInteger.Pow(10, decimals);
             var wholePart = BigInteger.DivRem(bigIntWei, divisor, out var remainder);
 
             var fractionalPart = (decimal)remainder / (decimal)divisor;
             return (decimal)wholePart + fractionalPart;
         }
 
-        /// <param name="decimalPlacesToUnit">Usually fetched from <see cref="CurrencyInfo.Decimals"/>.</param>
-        public static string ToRaw(decimal amount, int decimalPlacesToUnit)
+        /// <param name="amount">User-friendly coin amount that can have up to <paramref name="decimals"/> decimal places of precision.</param>
+        /// <param name="decimals">Number of decimal places allowed by the coin. Usually fetched from <see cref="CurrencyInfo.Decimals"/>.</param>
+        /// <returns>String containing numbers that represent the <paramref name="amount"/> in coin's atomic native units.</returns>
+        public static string ToRaw(decimal amount, int decimals)
         {
             var mantissa = (BigInteger)amount;
             var exponent = 0;
@@ -36,7 +41,7 @@ namespace Elympics.Util
                 --exponent;
                 num *= 10M;
             }
-            var multiplier = BigInteger.Pow(10, decimalPlacesToUnit + exponent);
+            var multiplier = BigInteger.Pow(10, decimals + exponent);
             var wei = BigInteger.Multiply(mantissa, multiplier);
             return wei.ToString();
         }

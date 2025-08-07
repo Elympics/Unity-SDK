b9216175837f85205675715f9d7164780ceb8754
Dawid Sygocki <dawid.sygocki@elympics.ai>
2025-08-07 11:31:54 +0200

    fix: Make MessagePack resolver used consistent between AOT and Editor, introduce a custom formatter for missing types

--- Runtime/Communication/Lobby/Models/ToLobby/RequestRollings.cs
+++ Runtime/Communication/Lobby/Models/ToLobby/RequestRollings.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Elympics.Lobby.Models;
 using MessagePack;
+using UnityEditor;
 
 namespace Communication.Lobby.Models.ToLobby
 {

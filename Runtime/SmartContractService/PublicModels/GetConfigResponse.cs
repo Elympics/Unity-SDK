af11471f0f38dd846767b6f1006bd593029da597
Krzysztof Pięta <krzysztof.pieta@elympics.cc>
2025-12-22 14:33:42 +0100

    fix: change SerializableField to Serializable attribute

--- Runtime/SmartContractService/PublicModels/GetConfigResponse.cs
+++ Runtime/SmartContractService/PublicModels/GetConfigResponse.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 namespace SCS
 {
-    [SerializeField]
+    [Serializable]
     public class GetConfigResponse
     {
         public int ChainId;

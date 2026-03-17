1d95345bf05febd279b01c6549ac2afac748233d
Dawid Sygocki <dawid.sygocki@elympics.ai>
2026-03-17 11:12:48 +0100

    chore: Fix incorrectly copied method definitions in RPC tests

--- Tests/Runtime/Common/RpcMocks/RpcHolderWithMetadata.cs
+++ Tests/Runtime/Common/RpcMocks/RpcHolderWithMetadata.cs
@@ -13,13 +13,13 @@ namespace Elympics.Tests.RpcMocks
         public void StpWithOptionalMetadataUsingDefault(RpcMetadata metadata = default) => StpWithOptionalMetadataArgs = new ValueTuple<RpcMetadata>(metadata);
 
         [ElympicsRpc(ElympicsRpcDirection.ServerToPlayers)]
-        public void StpWithOptionalMetadataUsingNew(RpcMetadata metadata = default) => StpWithOptionalMetadataArgs = new ValueTuple<RpcMetadata>(metadata);
+        public void StpWithOptionalMetadataUsingNew(RpcMetadata metadata = new()) => StpWithOptionalMetadataArgs = new ValueTuple<RpcMetadata>(metadata);
 
         [ElympicsRpc(ElympicsRpcDirection.PlayerToServer)]
         public void PtsWithOptionalMetadataUsingDefault(RpcMetadata metadata = default) => PtsWithOptionalMetadataArgs = new ValueTuple<RpcMetadata>(metadata);
 
         [ElympicsRpc(ElympicsRpcDirection.PlayerToServer)]
-        public void PtsWithOptionalMetadataUsingNew(RpcMetadata metadata = default) => PtsWithOptionalMetadataArgs = new ValueTuple<RpcMetadata>(metadata);
+        public void PtsWithOptionalMetadataUsingNew(RpcMetadata metadata = new()) => PtsWithOptionalMetadataArgs = new ValueTuple<RpcMetadata>(metadata);
 
         public void Reset()
         {

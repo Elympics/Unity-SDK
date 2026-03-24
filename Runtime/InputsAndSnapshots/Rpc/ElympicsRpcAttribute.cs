f23c571289a5a6168c2db09fec3b69ec63b8795c
Dawid Sygocki <dawid.sygocki@elympics.ai>
2026-03-24 09:42:10 +0100

    chore: Add XML docs to ElympicsRpcAttribute

--- Runtime/InputsAndSnapshots/Rpc/ElympicsRpcAttribute.cs
+++ Runtime/InputsAndSnapshots/Rpc/ElympicsRpcAttribute.cs
@@ -4,7 +4,9 @@ namespace Elympics
 {
     public enum ElympicsRpcDirection
     {
+        /// RPC is sent from a player instance to the server instance and executed there.
         PlayerToServer,
+        /// RPC is sent from the server instance to all player instances and executed there.
         ServerToPlayers,
     }
 
@@ -20,6 +22,11 @@ namespace Elympics
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
     public class ElympicsRpcAttribute : Attribute
     {
+        /// <summary>
+        /// After being received, should the RPC wait for the tick it was sent on?
+        /// If false, it will execute as soon as it arrives (but still wait for the correct part of the current tick loop).
+        /// </summary>
+        /// <value>true (by default)</value>
         public bool WaitForTick
         {
             get => Properties.WaitForTick;
@@ -28,6 +35,10 @@ namespace Elympics
 
         internal ElympicsRpcProperties Properties;
 
+        /// <summary>
+        /// Marks the method of <see cref="ElympicsMonoBehaviour"/> as a Remote Procedure Call.
+        /// </summary>
+        /// <param name="direction">Who is the sender and who is the addressee of the call?</param>
         public ElympicsRpcAttribute(ElympicsRpcDirection direction) =>
             Properties = new ElympicsRpcProperties(direction);
     }
